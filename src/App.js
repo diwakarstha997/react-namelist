f7af7c0fb21e34d19d79b7f50a5c0b777bd670aa
diwakarstha997 <165546734+diwakarstha997@users.noreply.github.com>
2025-01-24 08:23:20 +1100

    Add display on change function

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [name, setName] = useState(""); 
+
+  const handleOnChange = (e) => {
+    const { value } = e.target;
+    setName(value);
+  }; 
+
   return (
     <div className="wrapper"
     style={{ display: "flex",
@@ -15,11 +23,11 @@ function App() {
         padding: "2rem"
       }}
       >
-        <div className="display">As you are typing</div>
+        <div className="display">{name}</div>
 
         <div className="form">
           <form action="">
-            <input type="text" />
+            <input type="text" onChange={handleOnChange}/>
             <button>Add User</button>
           </form>
         </div>
